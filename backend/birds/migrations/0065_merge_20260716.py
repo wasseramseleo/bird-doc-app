5888842e395cac953d9930d31fc094324c07ccdc
Leonard Guelmino <leonard.guelmino@gmx.at>
2026-07-16 09:28:34 +0200

    fix(birds): merge migration reconciling 0064 leaf nodes after prd-370 merge

---
+++ backend/birds/migrations/0065_merge_20260716.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('birds', '0064_dataentry_is_dead_recovery_dataentry_is_non_standard'),
+        ('birds', '0064_speciesringsizeoverride'),
+    ]
+
+    operations = []
